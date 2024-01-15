98b5b7a6f0b16c86395ecd4a462f94cdd20ab79c
bernardDevDe <bernard14193@gmail.com>
2024-01-15 14:13:01 +0530

    login

--- frontend/src/app/(client)/login/page.js
+++ frontend/src/app/(client)/login/page.js
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="h-screen flex ">
+      <div className="w-full md:w-[400px] m-auto p-8 rounded-md shadow-0-0">
+        <img
+          src="../../../../assets/img/buyyinn-logo.png"
+          alt=""
+          className="mx-auto w-full mb-10"
+        />
+        <h1 className="text-center font-bold mb-8 text-2xl">Login</h1>
+        <form>
+          <div className="input flex flex-col mb-4">
+            <label htmlFor="">User ID</label>
+            <input type="email" className="border p-2 rounded mt-1" />
+          </div>
+          <div className="input flex flex-col mb-6">
+            <label htmlFor="">Password</label>
+            <input type="password" className="border p-2 rounded mt-1" />
+          </div>
+          <button
+            className="py-2 px-4 bg-violet-700 text-white mx-auto  rounded w-full text-center"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
