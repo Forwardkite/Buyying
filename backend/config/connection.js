93177371cc8d8a538659e069ea718627368e25de
SREERAJ <sreerajmack@gmail.com>
2024-02-11 21:14:33 +0530

    validation env

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,20 +1,4 @@
-// const mongoose = require("mongoose");
-
-// const connectionDB = async () => {
-//   try {
-//     await mongoose.connect(
-//       process.env.MONGODB_URI,
-//       {}
-//     );
-
-//     console.log("MongoDB connected successfully!");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error.message);
-//   }
-// };
-
-// module.exports = connectionDB;
-
+//global connection configuration
 const mongoose = require("mongoose");
 require('dotenv').config(); // Load environment variables from .env file
 
