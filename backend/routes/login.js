ad01dfe98069a8b44b453a6dded18ea22ec0c127
SREERAJ <sreerajmack@gmail.com>
2024-02-12 12:36:30 +0530

    cookie testing-#20

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -27,7 +27,7 @@ router.post('/', async (req, res) => {
 
         // Set the cookie in the response
         res.cookie('token', token, {
-            sameSite: 'none',
+            SameSite: 'none',
             secure: true
         });
         
