fc460845486afe11dd51b833f79e0ad14986ea8a
SREERAJ <sreerajmack@gmail.com>
2024-02-13 16:14:29 +0530

    cookie testing pos test#48

--- frontend/src/app/(client)/(login)/login/page.js
+++ frontend/src/app/(client)/(login)/login/page.js
@@ -30,7 +30,8 @@ export default function Login() {
         console.log('Token here:', token);
         // You can store the token in local storage or session storage for later use
         // localStorage.setItem('token', token);
-        Cookies.setItem('token', token);
+        Cookies.set('token', token, { expires: 1, secure: true, sameSite: 'none' });
+        window.location.href = "/";
       } else {
         console.error('Failed to fetch token');
       }
